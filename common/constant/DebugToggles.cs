821897b3983ed3b67fd844f56813a8d823a206db
Werti1304 <werti1304@gmail.com>
2025-03-14 17:21:46 +0100

    Removed all old rooms

--- common/constant/DebugToggles.cs
+++ common/constant/DebugToggles.cs
@@ -1,20 +1,8 @@
-#region *.hpp
-// //----------------------------------------------------------------------------------------------------------------------
-// // Debug.cs
-// //
-// // <Explanation of the file's contents ...>
-// // <... may span multiple lines.>
-// //
-// // Author: <name>, <name> ...
-// //----------------------------------------------------------------------------------------------------------------------
-// //
-#endregion
-
 namespace Flamme.common.constant;
 
 public class DebugToggles
 {
   // Toggles for debug outputs / visuals / settings
-  public const bool MinimapSeeAll = false;
-  public const bool DoorsAlwaysOpen = false;
+  public const bool MinimapSeeAll = true;
+  public const bool DoorsAlwaysOpen = true;
 }
