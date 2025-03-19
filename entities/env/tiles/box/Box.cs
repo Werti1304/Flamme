8b4f399f3a345b8db43a162c1004922f57bbe223
David Peinsipp <david.peinsipp@student.tugraz.at>
2025-03-19 13:38:48 +0100

    Added box

---
+++ entities/env/tiles/box/Box.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+using Flamme.entities;
+using Flamme.testing;
+
+public partial class Box : StaticBody2D, IPlayerDamageable
+{
+  [Export] public float StartHealth = 10;
+  private float _health;
+  
+  [ExportGroup("Textures")]
+  [Export] public Texture2D Texture1;
+  [Export] public Texture2D Texture2;
+  [Export] public Texture2D Texture3;
+  [Export] public Texture2D TextureBroken;
+  
+  [ExportGroup("Meta")]
+  [Export] public Sprite2D Sprite;
+  [Export] public CollisionShape2D CollisionShape;
+
+  public override void _Ready()
+  {
+    ExportMetaNonNull.Check(this);
+    
+    _health = StartHealth;
+
+    Update();
+  }
+
+  public float GetHealth()
+  {
+    return _health;
+  }
+
+  public void Hit(float attackDamage, float knockBackStrength, Vector2 attackDirection)
+  {
+    _health -= attackDamage;
+
+    Update();
+  }
+
+  private void Update()
+  {
+    var percent = _health / StartHealth;
+    if (percent >= 0.66f)
+    {
+      Sprite.Texture = Texture1;
+    }
+    else if (percent >= 0.33f)
+    {
+      Sprite.Texture = Texture2;
+    }
+    else if (percent > 0f)
+    {
+      Sprite.Texture = Texture3;
+    }
+    else
+    {
+      Sprite.Texture = TextureBroken;
+      CollisionShape.SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+    }
+  }
+}
